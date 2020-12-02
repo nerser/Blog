32bbd4b08303c9e93a667f5e3547e6fc28e837c4
Illia <shkiroillya@gmail.com>
2020-12-02 15:48:06 +0200

    Add Post model and templates index, detail

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-	names = ['Illia', 'Olya', 'Kolya']
-	return render(request, 'blog/index.html', context={'names': names})
+	posts = Post.objects.all()
+	return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+	post = Post.objects.get(slug__iexact=slug)
+	return render(request, 'blog/post_detail.html', context={'post': post})

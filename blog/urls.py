257540cef4f8ba7e7b94fd4b91a321b36aa43ed1
Illia <shkiroillya@gmail.com>
2020-12-02 17:31:18 +0200

    Add tags for posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<str:slug>/', views.post_detail, name='post_detail_url'),
+    path('tags/', views.tags_list, name='tags_list_url'),
+    path('tag/<str:slug>', views.tag_detail, name='tag_detail_url'),
 ]

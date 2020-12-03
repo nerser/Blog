0de2bd5c577f44d1a96a9322eeaf14d58409edf9
Illia <shkiroillya@gmail.com>
2020-12-03 16:40:01 +0200

    Add forms for adding list of tags

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<str:slug>/', views.PostDetail.as_view(), name='post_detail_url'),
     path('tags/', views.tags_list, name='tags_list_url'),
-    path('tag/<str:slug>', views.TagDetail.as_view(), name='tag_detail_url'),
+    path('tag/create/', views.TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>/', views.TagDetail.as_view(), name='tag_detail_url'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
+from .forms import TagForm
 
 
 # Create your views here.
@@ -19,6 +20,22 @@ class PostDetail(ObjectDetailMixin, View):
 class TagDetail(ObjectDetailMixin, View):
 	model = Tag
 	template = 'blog/tag_detail.html'
+
+
+class TagCreate(View):
+
+	def get(self, request):
+		form = TagForm()
+		return render(request, 'blog/tag_create.html', context={'form': form})
+
+	def post(self, request):
+		bound_form = TagForm(request.POST)
+
+		if bound_form.is_valid():
+			new_tag = bound_form.save()
+			return redirect(new_tag)
+
+		return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
 def tags_list(request):

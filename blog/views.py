6d9b9f1c8774a769451395def5967aba6ee1cdcf
Illia <shkiroillya@gmail.com>
2020-12-06 19:21:02 +0200

    Add pagination and searching features

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin, ObjectCreate, ObjectUpdate, ObjectDelete
@@ -9,8 +12,38 @@ from .forms import TagForm, PostForm
 
 # Create your views here.
 def post_list(request):
-	posts = Post.objects.all()
-	return render(request, 'blog/index.html', context={'posts': posts})
+	search_query = request.GET.get('search', '')
+
+	if search_query:
+		posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+	else:
+		posts = Post.objects.all()
+
+	paginator = Paginator(posts, 3)
+
+	page_number = request.GET.get('page', 1)
+	page = paginator.get_page(page_number)
+
+	is_paginated = page.has_other_pages()
+
+	if page.has_previous():
+		prev_url = f'?page={page.previous_page_number()}'
+	else:
+		prev_url = ''
+
+	if page.has_next():
+		next_url = f'?page={page.next_page_number()}'
+	else:
+		next_url = ''
+
+	context = {
+		'page_object': page,
+		'is_paginated': is_paginated,
+		'prev_url': prev_url,
+		'next_url': next_url
+	}
+
+	return render(request, 'blog/index.html', context=context)
 
 
 class PostDetail(ObjectDetailMixin, View):
@@ -18,21 +51,24 @@ class PostDetail(ObjectDetailMixin, View):
 	template = 'blog/post_detail.html'
 
 
-class PostCreate(ObjectCreate, View):
+class PostCreate(LoginRequiredMixin, ObjectCreate, View):
 	form_model = PostForm
 	template = 'blog/post_create_form.html'
+	raise_exception = True
 
 
-class PostUpdate(ObjectUpdate, View):
+class PostUpdate(LoginRequiredMixin, ObjectUpdate, View):
 	form_model = PostForm
 	model = Post
 	template = 'blog/post_update_form.html'
+	raise_exception = True
 
 
-class PostDelete(ObjectDelete, View):
+class PostDelete(LoginRequiredMixin, ObjectDelete, View):
 	model = Post
 	template = 'blog/post_delete_form.html'
 	model_list_template = 'post_list'
+	raise_exception = True
 
 
 class TagDetail(ObjectDetailMixin, View):
@@ -40,21 +76,24 @@ class TagDetail(ObjectDetailMixin, View):
 	template = 'blog/tag_detail.html'
 
 
-class TagCreate(ObjectCreate, View):
+class TagCreate(LoginRequiredMixin, ObjectCreate, View):
 	form_model = TagForm
 	template = 'blog/tag_create.html'
+	raise_exception = True
 
 
-class TagUpdate(ObjectUpdate, View):
+class TagUpdate(LoginRequiredMixin, ObjectUpdate, View):
 	form_model = TagForm
 	model = Tag
 	template = 'blog/tag_update_form.html'
+	raise_exception = True
 
 
-class TagDelete(ObjectDelete, View):
+class TagDelete(LoginRequiredMixin, ObjectDelete, View):
 	model = Tag
 	template = 'blog/tag_delete_form.html'
 	model_list_template = 'tags_list_url'
+	raise_exception = True
 
 
 def tags_list(request):
